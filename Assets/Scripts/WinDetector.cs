3363819a736cf28f46f9bff7cb9381534a232fa0
Yuvraj Singh Bisht <yuvrajbisht33@gmail.com>
2017-03-23 15:41:50 +0530

    Compelete Project

---
+++ Assets/Scripts/WinDetector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinDetector : MonoBehaviour {
+
+	public int score = 0;
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.CompareTag ("enemy")) {
+			Destroy (col.gameObject);
+			score += 1;
+		} 
+		else if (col.gameObject.CompareTag ("Player")) {
+            //Application.LoadLevel ("gameOver");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("gameOver");
+        } else if (col.gameObject.CompareTag ("goodGuy")) {
+            //Application.LoadLevel ("gameOver");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("gameOver");
+        }
+	}
+}
---
+++ Assets/Scripts/gameOver.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameOver : MonoBehaviour {
+
+	public Texture gameOverTexture;
+	public GameObject WinDetector;
+	public int score;
+
+	void Start () {
+		WinDetector = GameObject.Find ("WinDetector");
+		score = WinDetector.GetComponent<WinDetector>().score;
+	}
+
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),gameOverTexture);
+		GUI.TextArea (new Rect (0, 0, Screen.width/4, Screen.height/4), "Score: " + score);
+		if (GUI.Button(new Rect(Screen.width / 2, Screen.height /2+20, 80, 25),"Play Again")) 
+		{
+            //Application.LoadLevel("minigame");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("minigame");
+        }
+		if (GUI.Button(new Rect(Screen.width / 2, Screen.height /2 + 45, 80, 25),"Exit")) 
+		{
+			Application.Quit();
+		}
+	}
+}
